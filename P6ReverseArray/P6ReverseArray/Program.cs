b67de483221e76fcefefc65a178c100072020d16
1l1y4N <cvetkov.iliyan@gmail.com>
2017-06-13 09:43:40 -0700

    All done

---
+++ P6ReverseArray/P6ReverseArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace P6ReverseArray
+{
+    class P06_ReverseArrayOfStrings
+    {
+        static void Main(string[] args)
+        {
+            string[] inputStrArrReverced = Console.ReadLine()
+                .Split(' ')
+                .ToArray()
+                .Reverse()
+                .ToArray();
+            Console.WriteLine(string.Join(" ", inputStrArrReverced));
+        }
+    }
+}
